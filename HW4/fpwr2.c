42386e458d551626c20f39010b63cacfcf7e2836
shirleyhxm <hexuemin@g.ucla.edu>
2016-11-29 16:47:59 -0800

    Add files via upload

---
+++ HW4/fpwr2.c
@@ -0,0 +1,31 @@
+extern double u2f(unsigned long);
+
+double fpwr2(int x)
+{
+  /* Result exponent and fraction */
+  unsigned long exp, frac;
+  unsigned long u;
+
+  if (x < -1074) {
+    /* Too small. Return 0.0 */
+    exp = 0;
+    frac = 0;
+  } else if (x < －1022) {
+    /* Denormalized result */
+    exp = 1;
+    frac = 1 << (x+1074);
+  } else if (x < 1024) {
+    /* Normalized result. */
+    exp = x+1023;
+    frac = 0;
+  } else {
+    /* Too big. Return +inf */
+    exp = 2047;
+    frac = 0;
+  }
+
+  /* Pack exp and frac into 64 bits */
+  u = exp << 52 | frac;
+  /* Return as double */
+  return u2f(u);
+}
